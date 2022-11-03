efbe6ccc9812f90b97eed468d4d5473e86de4eb2
Sai-7788 <saisanta7013@gmail.com>
2022-11-03 19:31:01 +0530

    okkk

--- react-9/src/Comp/Component.jsx
+++ react-9/src/Comp/Component.jsx
@@ -6,7 +6,7 @@ class Component extends React.Component{
                 <div className="row">
                 <div className="col-6">
                     <h3>Allu Arjun (born 8 April 1982) is an Indian actor who works in Telugu films. One of the highest paid actors in India,[3] Arjun is also known for his dancing.[4] He is a recipient of several awards, including five Filmfare Awards South and five Nandi Awards.[5]
-                         Allu Arjun made his debut with Gangotri in 2003. He rose to prominence starring in Sukumar's cult classic Arya (2004), for which he earned a Nandi Special Jury Award.[6] He consolidated his reputation with the action films Bunny (2005) and Desamuduru (2007). In 2008, he starred in the romantic drama Parugu, for which he won his first Filmfare Award for Best Actor – Telugu.</h3>
+                         Allu Arjun have made his debut with Gangotri in 2003. He rose to prominence starring in Sukumar's cult classic Arya (2004), for which he earned a Nandi Special Jury Award.[6] He consolidated his reputation with the action films Bunny (2005) and Desamuduru (2007). In 2008, he starred in the romantic drama Parugu, for which he won his first Filmfare Award for Best Actor – Telugu.</h3>
                 </div>
             <div className="col-6">
             <img src="https://www.thenewsminute.com/sites/default/files/styles/news_detail/public/AlluArjun_290321_1200_TNM_0.jpg?itok=m85PhmAU"/>
